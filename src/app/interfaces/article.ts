c4294af1c2e215bca25a017189d7e0a37cb985cf
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-11-17 21:24:26 -0500

    completed home page

---
+++ src/app/interfaces/article.ts
@@ -0,0 +1,10 @@
+export interface Article {
+	title: String;
+	image: String;
+	description?: String;
+	content: String;
+	createdBy: String;
+	updatedBy: String;
+	createdAt: Date;
+	updatedAt: Date;
+}
